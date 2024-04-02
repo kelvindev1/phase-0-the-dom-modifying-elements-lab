fe46de46ef731c309b25aa244121636da1ddd727
kelvindev1 <kelvinmutugi336@gmail.com>
2024-04-02 11:56:17 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your code here!
+const mainElement = document.getElementById('main');
+mainElement.parentNode.removeChild(mainElement);
+
+let newHeader = document.createElement('h1');
+newHeader.textContent = "This is the new header";
+
+
+document.body.appendChild(newHeader)
+
+newHeader = document.createElement('h1');
+newHeader.textContent = "This is the new header";
+
+
+newHeader.id = 'victory';
+
+
+document.body.appendChild(newHeader);
+
+newHeader = document.createElement('h1');
+newHeader.textContent = "kelvin mutugi is the champion";
+
+newHeader.id = 'victory';
+
+document.body.appendChild(newHeader);
